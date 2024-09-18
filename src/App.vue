f42b8dee40fb5db0564a0e905fdf2117f2458bc5
Hakushin <kpx1164@gmail.com>
2024-09-18 23:07:53 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <section class="w-screen bg-pink-500 text-white h-screen items-center justify-center flex font-semibold ">Initialised</section>
+</template>
+
+<style scoped>
+
+</style>
