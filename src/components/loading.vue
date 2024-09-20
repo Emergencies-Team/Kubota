efe3ee51001c35742a27b2dde5fdf4477e2f94c8
Hakushin <kpx1164@gmail.com>
2024-09-20 13:09:51 +0700

    compress video quanlity

--- src/components/loading.vue
+++ src/components/loading.vue
@@ -3,7 +3,7 @@
 
 <template>
   <div class="relative w-screen h-screen">
-    <video src="../assets/video/loading.mp4" autoplay muted class="absolute inset-0 w-full h-full object-cover"></video>
+    <video src="../assets/video/low_loading.mp4" autoplay muted class="absolute inset-0 w-full h-full object-cover"></video>
   </div>
 </template>
 
